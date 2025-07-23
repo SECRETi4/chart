a9b67c2e106acfb103301335b114a7ff6fbfc23e
SECRETi4 <ulugbek2011not@gmail.com>
2025-07-23 16:03:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#ism", {
+      strings: ["Ulug'bek  Abdusamatov"],
+      typeSpeed: 150,
+    });
+var typed1 = new Typed("#p", {
+      strings: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident voluptas facilis officia esse sequi rerum, corporis ex ut natus minima?"],
+      typeSpeed: 100,
+    });
